b9ad41bd610ed133dcd86a98e335ce5d18dea9d7
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-07-30 15:55:51 +0100

    07 Pointer

---
+++ 07Pointer/src/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstdlib>
+
+void update(int *a,int *b) {
+	int aux = *a;
+
+	*a += *b;
+
+	*b -= aux;
+
+	*b = abs(*b);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
